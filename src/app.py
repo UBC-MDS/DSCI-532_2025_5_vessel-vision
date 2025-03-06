3a7958ca5b28bbc8ce76cebaf4dffb0b1a986b05
AzinPiran <az.p.ctc@gmail.com>
2025-03-06 13:10:15 -0800

    App.py modified to fix the bug

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
+#main
 import os
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html, dash_table
 from flask import Flask
-import plotly.graph_objects as go
 import sys
-
+import plotly.graph_objects as go
 # Add the current directory to sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,40 +21,18 @@ app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Load AIS dataset
 df = load_data(date_filter="2024-01-01")
+#df = df.head(100)
 
 # Ensure consistent date format
 df['Hour'] = df['BaseDateTime'].dt.hour
 df['BaseDateTime'] = pd.to_datetime(df['BaseDateTime']).dt.strftime('%Y-%m-%d')
+
 
 # Function to create Bootstrap-styled Port table
 df_port = calculate_arrivals_departures(df)
 
 def create_port_table(df_port):
     """Create a Bootstrap Card containing the Port Table."""
-    vessel_types = df['Vessel Type Name'].dropna().unique()
-    
-    # Creating a list of Vessel Type Name and its color (this could be customized)
-    vessel_type_colors = {
-        "Cargo": "#FF6347",
-        "Fishing": "#4682B4",
-        "Passenger": "#32CD32",
-        "Tanker": "#FFD700"
-    }
-    
-    # Adding color information to the header
-    header_cells = [
-        {
-            "name": f"Vessel Type Name ({vessel_type})",
-            "id": vessel_type,
-            "style": {
-                "backgroundColor": vessel_type_colors.get(vessel_type, "#808080"),  # Default gray if type is missing
-                "color": "white",
-                "fontWeight": "bold"
-            }
-        }
-        for vessel_type in vessel_types
-    ]
-    
     return dbc.Card(
         dbc.CardBody([ 
             html.H5("Number of Arrivals & Departures per Port", style={"fontFamily": "Arial, sans-serif"}),
@@ -64,24 +42,11 @@ def create_port_table(df_port):
                 data=df_port.to_dict("records"),
                 page_size=5,
                 style_table={"overflowX": "auto", "margin": "auto", "border": "none", "fontFamily": "Arial, sans-serif"},
-                style_header={
-                    "fontWeight": "bold", 
-                    "border": "none", 
-                    "fontFamily": "Arial, sans-serif",
-                    "backgroundColor": "#D3D3D3",  # Light gray background for header
-                },
-                style_cell={ 
-                    "textAlign": "center", 
-                    "border": "none", 
-                    "fontFamily": "Arial, sans-serif"
-                },
-                style_data={"border": "none"},
-                style_header_conditional=header_cells  # Apply conditional styling for coloring
+                style_header={"fontWeight": "bold", "border": "none", "fontFamily": "Arial, sans-serif"},
+                style_cell={"textAlign": "center", "border": "none", "fontFamily": "Arial, sans-serif"},
+                style_data={"border": "none"}
             )
-        ]),
-        style={
-            "flex": "1", "display": "flex", "flexDirection": "column"  
-        }
+        ])
     )
 
 # Function to create Bootstrap-styled summary cards
@@ -98,18 +63,77 @@ def create_summary_card(title, value, color):
 def create_footer():
     return dbc.Container(
         dbc.Row(
-            dbc.Col([ 
+            dbc.Col([
                 html.Hr(), 
                 html.P("Vessel Vision, extract and process AIS data for maritime traffic analysis.", className="text-center", style={"fontWeight": "bold"}),
                 html.P("Developed by [DSCI-532-group5]: Azin Piran, Stephanie Wu, Yasmin Hassan, Zoe Ren", className="text-center"),
-                html.P([ 
-                    "GitHub Repository: ",
-                    html.A("Vessel Vision", href="https://github.com/UBC-MDS/DSCI-532_2025_5_vessel-vision", target="_blank")
-                ], className="text-center"),
+                html.P([
+                    "GitHub Repository: " ,
+                    html.A("Vessel Vision", href="https://github.com/UBC-MDS/DSCI-532_2025_5_vessel-vision", target="_blank")],
+                    className="text-center"
+                ),
                 html.P(f"Last updated: 2025-03-01 ", className="text-center"),
             ])
         ), className="mt-4"
     )
+
+# # App layout
+# app.layout = dbc.Container([
+#     html.H1("Vessel Vision - 🚢 AIS Unique Vessel Tracking", className="text-center my-4"),
+
+#     # Summary Metrics Row (Now Includes Maximum Time Anchored)
+#     dbc.Row([
+#         dbc.Col(create_summary_card("Total Unique Vessels", "total-unique-vessels", "#007BFF"), width=3),
+#         dbc.Col(create_summary_card("Total Moving Vessels", "total-moving-vessels", "#28A745"), width=3),
+#         dbc.Col(create_summary_card("Total Anchored Vessels", "total-anchored-vessels", "#DC3545"), width=3),
+#         dbc.Col(create_summary_card("Max Time Anchored (hours)", "max-time-anchored", "#FFC107"), width=3),
+#     ], className="justify-content-center my-3"),
+
+#     # Filters Section
+#     dbc.Row([
+#         dbc.Col(dcc.Dropdown(
+#             id="vessel-type-filter",
+#             options=[{"label": vessel_type, "value": vessel_type} for vessel_type in df['Vessel Type Name'].dropna().unique()],
+#             placeholder="Select Vessel Type"
+#         ), width=3),
+
+#         dbc.Col(dcc.Dropdown(
+#             id="nearest-port-filter",
+#             options=[{"label": port, "value": port} for port in df['Nearest Port'].dropna().unique()],
+#             placeholder="Select Nearest Port"
+#         ), width=3),
+
+#         dbc.Col(dcc.RadioItems(
+#             id="date-filter",
+#             options=[{"label": date, "value": date} for date in df['BaseDateTime'].dropna().unique()],
+#             value=df['BaseDateTime'].min(),
+#             inline=True
+#         ), width=3)
+#     ], className="my-3"),
+
+#     # Port data Section 
+#     dbc.Row([
+#         dbc.Col(create_port_table(df_port), width=4, style={"height": "100%"}),
+
+#         # Map Section with border, filling the height
+#         dbc.Col(
+#             dcc.Graph(
+#                 id="map-output", 
+#                 style={
+#                     'height': '100%',  # Ensure full height for the map
+#                     'border': '2px solid #ddd',  # Add a light grey border
+#                     'borderRadius': '5px'  # Optional: round corners of the border
+#                 }
+#             ), 
+#             width=8,
+#             style={"height": "100%"}  # Ensure map column also takes up full available height
+#         ),
+#     ], align="stretch", className="my-3"),
+
+#     # Footer section
+#     create_footer()
+
+# ], fluid=True)
 
 # Add a trend line graph function
 def create_trend_graph(df):
@@ -146,8 +170,6 @@ def create_trend_graph(df):
         ]),
         style={"flex": "1", "display": "flex", "flexDirection": "column"}  # Expands to fill space
     )
-
-
 # App layout
 ###################################################################################################################################
 # App layout
@@ -226,6 +248,11 @@ app.layout = dbc.Container([
 
 
 #######################################################################################################################################
+
+
+# Register callbacks to update the map & summary stats
+register_callbacks(app, df)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run_server(host="0.0.0.0", port=port)
