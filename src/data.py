4b6945bb668caba0f11b05b62cf6e800c6a27c0a
AzinPiran <az.p.ctc@gmail.com>
2025-03-16 05:04:38 -0700

    Data filtered for 13 to 18 pm

--- src/data.py
+++ src/data.py
@@ -23,7 +23,7 @@ def load_data(date_filter=None):
 
     # Filter for times between 13:00 and 13:30
     combined_df = combined_df[(combined_df['BaseDateTime'].dt.time >= pd.to_datetime("13:00").time()) &
-                            (combined_df['BaseDateTime'].dt.time <= pd.to_datetime("13:30").time())]
+                            (combined_df['BaseDateTime'].dt.time <= pd.to_datetime("18:00").time())]
 
 
 
